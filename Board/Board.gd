3e06fda2ffe34df2ab2c2c9d146ebbc1be95d444
unknown <624665+tcfitzgerald@users.noreply.github.com>
2022-11-10 18:47:47 -0600

    Starting on the main game board

---
+++ Board/Board.gd
@@ -0,0 +1,13 @@
+extends Control
+class_name Board
+
+var card_offset: int = 50
+var tableau_count: int = 5
+var overflow_tableau_count: int = 2
+
+var available_moves: Array = []
+var playable_cards: Array = []
+
+
+func _ready() -> void:
+	pass

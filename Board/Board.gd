8462a3e8adc8630284227c387f6d456d54ddc816
unknown <624665+tcfitzgerald@users.noreply.github.com>
2022-11-11 19:02:11 -0600

    Adding WastePile and building out structure of board

--- Board/Board.gd
+++ Board/Board.gd
@@ -4,6 +4,7 @@ class_name Board
 var card_offset: int = 50
 var tableau_count: int = 5
 var overflow_tableau_count: int = 2
+var cards_per_tableau: int = 1
 
 var available_moves: Array = []
 var playable_cards: Array = []
